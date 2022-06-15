3ea095d4e122b01e65c53d613bdda4949748bcb7
iPanda <aungkyawkhaing2000@gmail.com>
2022-06-15 07:46:24 +0630

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { Button,Typography,styled } from '@mui/material'
+import {Add,Settings} from '@mui/icons-material'
+const App = () => {
+   const BlueButton = styled(Button)({
+    backgroundColor:"skyblue",
+    color:'#888',
+    margin:"25px",
+    "&:hover":{
+      backgroundColor:"lightblue",
+    
+    },
+    "&:disabled":{
+      backgroundColor:"gray",
+      color:"white"
+    }
+   })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div>
+        <Button variant="text">Text</Button>
+        <Button startIcon={<Settings/>} variant="contained" color="secondary" size='small'>setting</Button>
+        <Button startIcon={<Add/>} variant="contained" color="success" size='small'>Add new Post</Button>
+        <Button variant="outlined">Outlined</Button>
+         <Typography variant='h1' component="p">
+             Its uses h1 style but  it's a p Tag
+         </Typography>
+        <BlueButton>My button</BlueButton>
+        <BlueButton>My another button</BlueButton>
+     </div>
+  )
 }
 
-export default App;
+export default App
